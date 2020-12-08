328cf8307809bf7d90d5e026836beba38f73df51
Jonathan Koh <jontkoh@gmail.com>
2020-12-07 18:46:02 -0800

    initial commit for react and express skeleton code created from scratch updated

---
+++ README.md
@@ -0,0 +1,1 @@
+<h3 align="center">App to get an idea of how react and express connect to each other</h3>
---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const htmlWebpackPlugin = require("html-webpack-plugin"); 
+const path = require("path");
+
+module.exports = {
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+    ]
+  },
+};
